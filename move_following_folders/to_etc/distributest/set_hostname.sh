268f5385e24541fd874fa4ca9796f634162aa0d0
Curtis Carter <curtis@rubyhq.com>
2011-01-25 00:16:44 +0530

    Notes for settings

--- move_following_folders/to_etc/distributest/set_hostname.sh
+++ move_following_folders/to_etc/distributest/set_hostname.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
+#Replace fully qualified hostname here
+#This is used to start the vms with proper hostnames as most boxes do not have them set locally
 export DISTRIBUTEST_LOCAL_HOSTNAME=#PUT HOSTNAME HERE!!!!!
